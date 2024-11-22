c58306f8c67e9b8c66a995d5633cfb93069c16e0
su2708 <46125751+su2708@users.noreply.github.com>
2024-11-22 22:49:08 +0900

    Create home.py

---
+++ home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="RAG & Agent Home",
+    page_icon="🤖"
+)
+
+st.markdown(
+    """
+    # Hello!
+    
+    Welcome to my RAG & Agent practice!
+    
+    Here are the apps I made:
+    
+    - [ ] [AI News](/AI_News)
+    - [ ] [Youtube AI](/Youtube_AI)
+    - [ ] [Whisper](/Whisper)
+    """
+)
